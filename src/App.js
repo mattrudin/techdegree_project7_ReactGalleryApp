3442c6621561463dfab3152cea71ff5f4527ab9f
Matthias Rudin <matthias.rudin@live.com>
2019-03-06 20:10:28 +0100

    Add blank page at first load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 import apiKey from './config';
 import Header from './components/Header';
@@ -8,13 +7,15 @@ import Gallery from './components/Gallery'
 
 class App extends Component {
   state = {
-    images: []
+    images: [],
+    initialState: true
   }
 
   performSearch = query => {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=817f1e338c444ff681b9b3ebbe4836a6&text=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(res => this.setState({
-        images: res.data.photos.photo
+        images: res.data.photos.photo,
+        initialState: false
       }))
       .catch(err => console.log('Error fetching and parsing data:', err))
   }
@@ -23,7 +24,9 @@ class App extends Component {
     return (
       <div className="container">
         <Header onSearch={this.performSearch} />
-        <Gallery pictures={this.state.images} />
+        <Gallery 
+          pictures={this.state.images}
+          initialState={this.state.initialState} />
       </div>
     );
   }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,16 +3,26 @@ import GalleryItem from './GalleryItem';
 import NoResult from './NoResult';
 
 const Gallery = props => {
-  const { pictures } = props;
-  let result = pictures.length > 0 ? pictures.map(picture => <GalleryItem data={picture} key={picture.id}/>) : <NoResult />
+  const { initialState, pictures } = props;
+
+  let result;
+  if(initialState) {
+    result = null;
+  } else if(pictures.length > 0) {
+    result =  <>
+                <h2>Results</h2>
+                <ul>
+                  {pictures.map(picture => <GalleryItem data={picture} key={picture.id}/>)}
+                </ul>
+              </>  
+  } else {
+    result = <NoResult />
+  }
   
   return(
-    <div class="photo-container">
-        <h2>Results</h2>
-        <ul>
-          {result}
-        </ul>
-      </div>
+    <div className="photo-container">
+      {result}
+    </div>
   )
 }
 
