ec754e39264fa1ebfef744bcf7dc1ef2b67a1fd8
Matthias Rudin <matthias.rudin@live.com>
2019-03-06 20:28:20 +0100

    Add headline and font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 import SearchForm from './SearchForm';
 import Navigation from './Navigation';
 
 const GalleryItem = props => (
     <>
+        <h1 className="title">Find A Pic!</h1>
+        <h2>Your first place to find a pic</h2>
         <SearchForm onSearch={props.onSearch}/>
         <Navigation />
     </>
